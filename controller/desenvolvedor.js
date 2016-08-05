cce344272234d8986337ae25438b425f9637bf07
SamuelCarneiroFCM <samuel.a.carneiro@gmail.com>
2016-08-05 17:01:44 -0300

    ajustes

--- controller/desenvolvedor.js
+++ controller/desenvolvedor.js
@@ -4,7 +4,8 @@ module.exports = function(app){
 
 	var DesenvolvedorController = {
 		novo: function(req,res){
-			  res.render('home/novo', {dev: new Desenvolvedor()});
+			  console.log('aqui');
+			  res.render('home/novo');
 		},
 
 		post: function(req,res){
@@ -13,25 +14,25 @@ module.exports = function(app){
 				esquema.nome     = req.body.nome;
 				esquema.email    = req.body.email;
 				esquema.senha    = esquema.generateHash(req.body.senha);
-
 				Desenvolvedor.findOne({'email': esquema.email}, function(err,data){
 					if(data){
 						req.flash('erro', 'E-mail encontra-se cadastrado, tente outro.');
-						res.render('home/novo', {dev: esquema});
+						res.render('home/novo', {'dev': esquema});
 					}else{
 						esquema.save(function(err){
 							if(err){
-								req.flash('erro', 'Erro ao cadastrar: '+err);
-								res.render('home/novo', {dev: esquema});
+								req.flash('erro', 'Erro ao cadastrar: ' + err);
+								res.render('home/novo', {'dev': esquema});
 							}else{
 								req.flash('info', 'Registro cadastrado com sucesso!');
-								res.render('/');
+								req.session.desenvolvedor = data;
+								res.render('home/index', {'dev': esquema});
 							}
 						});
 					}
 				});
 			}else{
-				res.render('home/novo', {dev: esquema});
+				res.render('home/novo', {'dev': esquema});
 			}
 		}
 	}
